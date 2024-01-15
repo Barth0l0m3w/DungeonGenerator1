0e940300c82f2c61f48a14318ca610a7edf84181
Nathalie <xnhogenbergx@gmail.com>
2024-01-15 16:57:18 +0100

    room behaviour
    removing doors where the walls will be placed and the other way around

--- Assets/scripts/DungeonGenerator.cs
+++ Assets/scripts/DungeonGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scripts : MonoBehaviour
+public class DungeonGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
