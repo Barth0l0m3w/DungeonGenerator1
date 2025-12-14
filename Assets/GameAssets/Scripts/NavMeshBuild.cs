d64ac00cf308eb74b07e325172c7de07f4caa40b
Nathalie <xnhogenbergx@gmail.com>
2025-12-14 14:36:12 +0100

    ...

--- Assets/GameAssets/Scripts/NavMeshBuild.cs
+++ Assets/GameAssets/Scripts/NavMeshBuild.cs
@@ -1,19 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.AI.Navigation;
-using Unity.AI;
 using NaughtyAttributes;
-using UnityEngine.Serialization;
 
 public class NavMeshBuild : MonoBehaviour
 {
-    [FormerlySerializedAs("navSurvice")] public NavMeshSurface navService;
+    public NavMeshSurface navService;
     
     [Button]
     public void BuildNavigation()
     {
         navService.RemoveData();//clear the old bake
         navService.BuildNavMesh();//make a new bake according to the new dungeon
+    }
+
+    public void ClearOldNav()
+    {
+        navService.RemoveData();
     }
 }
